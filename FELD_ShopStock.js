865d959aa0b4e9cfa08856292ecc07161d45a10d
Feldherren <rpaliwoda@googlemail.com>
2017-10-06 22:04:16 +0100

    Condensed means of setting stock.

--- FELD_ShopStock.js
+++ FELD_ShopStock.js
@@ -42,23 +42,17 @@ contacted if you do use it in any games, just to know.
 			if (!(args[0] in shopStock))
 			{
 				shopStock[args[0]] = Object();
-				shopStock[args[0]]['items'] = Object();
-				shopStock[args[0]]['weapons'] = Object();
-				shopStock[args[0]]['armor'] = Object();
+				shopStock[args[0]]['itemStock'] = Object();
+				shopStock[args[0]]['weaponStock'] = Object();
+				shopStock[args[0]]['armorStock'] = Object();
+				shopStock[args[0]]['itemBaseStock'] = Object();
+				shopStock[args[0]]['weaponBaseStock'] = Object();
+				shopStock[args[0]]['armorBaseStock'] = Object();
 			}
+			// TODO: make sure args[1] is 'item', 'weapon' or 'armor' exactly
 			// keep track of original stock with second object (for each of items, weapons, armor) storing original stock amount?
-			if(args[1].toLowerCase() = 'item')
-			{
-				shopStock[args[0]]['items'][args[2]] = parseInt(args[3]);
-			}
-			if(args[1].toLowerCase() = 'weapon')
-			{
-				shopStock[args[0]]['weapons'][args[2]] = parseInt(args[3])
-			}
-			if(args[1].toLowerCase() = 'armor')
-			{
-				shopStock[args[0]]['armor'][args[2]] = parseInt(args[3])
-			}
+			shopStock[args[0]][args[1]+'Stock'][args[2]] = parseInt(args[3]);
+			shopStock[args[0]][args[1]+'BaseStock'][args[2]] = parseInt(args[3]);
 		}
 		else if(command == "change_stock" && args[0] != null && args[1] != null && args[2] != null && args[3] != null)
 		{
@@ -68,20 +62,12 @@ contacted if you do use it in any games, just to know.
 				shopStock[args[0]]['items'] = Object();
 				shopStock[args[0]]['weapons'] = Object();
 				shopStock[args[0]]['armor'] = Object();
+				shopStock[args[0]]['itemBaseStock'] = Object();
+				shopStock[args[0]]['weaponBaseStock'] = Object();
+				shopStock[args[0]]['armorBaseStock'] = Object();
 			}
-			
-			if(args[1].toLowerCase() = 'item')
-			{
-				shopStock[args[0]]['items'][args[2]] += parseInt(args[3]);
-			}
-			if(args[1].toLowerCase() = 'weapon')
-			{
-				shopStock[args[0]]['weapons'][args[2]] += parseInt(args[3])
-			}
-			if(args[1].toLowerCase() = 'armor')
-			{
-				shopStock[args[0]]['armor'][args[2]] += parseInt(args[3])
-			}
+			// TODO: make sure args[1] is 'item', 'weapon' or 'armor' exactly
+			shopStock[args[0]][args[0]+'Stock'][args[2]] = parseInt(args[3]);
 		}
 	}
 })();
