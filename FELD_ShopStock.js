9626150c676a5de1f9c8dd0c85233a59fd461f4e
Feldherren <rpaliwoda@googlemail.com>
2017-10-07 17:59:06 +0100

    Now displays stock in shop interface, after the item name. Various commands now work.

--- FELD_ShopStock.js
+++ FELD_ShopStock.js
@@ -2,6 +2,10 @@
  * @plugindesc Implements limited stock for shops.
  * @author Feldherren
  * @help Shop Stock v1.0.0, by Feldherren (rpaliwoda AT googlemail.com)
+ *
+ * @param Refresh resets stock to base levels
+ * @desc Whether refresh resets stock to base levels (true) or adds base stock to current stock (false); true/false; NOTE NOT FUNCTIONAL YET
+ * @default true
  
 Changelog:
 1.0: initial release
@@ -60,44 +64,114 @@ Where stock has not been defined, should the shop act as usual with infinite sto
 	
 	function refreshShop(shopName)
 	{
+		//console.log("refreshing stock for " + shopName);
 		if (shopName in shopStock)
 		{
-			for (var item in shopStock[shopName]['itemStock'])
+			//console.log(shopName + " is in shopStock");
+			//console.log(shopStock[shopName]['itemStock']);
+			for (var item in shopStock[shopName]['itemBaseStock'])
 			{
-				shopStock[shopName]['itemStock'][item] = shopStock[shopName]['itemBaseStock'][item];
+				//console.log(item);
+				setCurrentStock(shopName, 'item', item, shopStock[shopName]['itemBaseStock'][item]);
 			}
-			for (var weapon in shopStock[shopName]['weaponStock'])
+			for (var weapon in shopStock[shopName]['weaponBaseStock'])
 			{
-				shopStock[shopName]['weaponStock'][weapon] = shopStock[shopName]['weaponBaseStock'][weapon];
+				//shopStock[shopName]['weaponStock'][weapon] = shopStock[shopName]['weaponBaseStock'][weapon];
+				setCurrentStock(shopName, 'weapon', weapon, shopStock[shopName]['itemBaseStock'][item]);
 			}
-			for (var armor in shopStock[shopName]['armorStock'])
+			for (var armor in shopStock[shopName]['armorBaseStock'])
 			{
-				shopStock[shopName]['armorStock'][armor] = shopStock[shopName]['armorBaseStock'][armor];
+				//shopStock[shopName]['armorStock'][armor] = shopStock[shopName]['armorBaseStock'][armor];
+				setCurrentStock(shopName, 'armor', armor, shopStock[shopName]['itemBaseStock'][item]);
 			}
 		}
+	}
+	
+	// refreshes ALL shops currently defined
+	function refreshAllShops()
+	{
 	}
 	
 	function setCurrentStock(shopName, itemType, itemId, stock)
 	{
 		if (itemType == 'item' || itemType == 'weapon' || itemType == 'armor')
 		{
-			shopStock[shopName][itemType+'Stock'][itemId] = parseInt(stock);
+			shopStock[shopName][itemType+'Stock'][parseInt(itemId)] = parseInt(stock);
 		}
+	}
+	
+	function getCurrentStock(shopName, item)
+	{
+		stock = undefined;
+		
+		//console.log(shopStock);
+		
+		if (shopName != null)
+		{
+			console.log("getting current stock for " + item.name + " in " + shopName);
+			if (DataManager.isItem(item)) // item
+			{
+				//console.log(item.name + " is an item");
+				stock = shopStock[shopName]['itemStock'][item.id];
+				//console.log(shopName + " has " + stock + " " + item.name);
+			}
+			else if (DataManager.isWeapon(item)) // weapon
+			{
+				//console.log(item.name + " is a weapon");
+				stock = shopStock[shopName]['weaponStock'][item.id];
+				//console.log(shopName + " has " + stock + " " + item.name);
+			}
+			else if (DataManager.isArmor(item)) // armor
+			{
+				//console.log(item.name + " is armor");
+				stock = shopStock[shopName]['armorStock'][item.id];
+				//console.log(shopName + " has " + stock + " " + item.name);
+			}
+		}
+		
+		return stock;
 	}
 	
 	function setBaseStock(shopName, itemType, itemId, stock)
 	{
 		if (itemType == 'item' || itemType == 'weapon' || itemType == 'armor')
 		{
-			shopStock[shopName][itemType+'BaseStock'][itemId] = parseInt(stock);
+			shopStock[shopName][itemType+'BaseStock'][parseInt(itemId)] = parseInt(stock);
 		}
+	}
+	
+	// may not be useful in this form, if this needs the item
+	function getBaseStock(shopName, item)
+	{
+		var stock = undefined;
+		if (shopName != null)
+		{
+			console.log("getting base stock for " + item.name + " in " + shopName);
+			if (DataManager.isItem(item)) // item
+			{
+				console.log(item.name + " is an item");
+				stock = shopStock[currentShop]['itemBaseStock'][item.id];
+			}
+			else if (DataManager.isWeapon(item)) // weapon
+			{
+				console.log(item.name + " is a weapon");
+				stock = shopStock[currentShop]['weaponBaseStock'][item.id];
+			}
+			else if (DataManager.isArmor(item)) // armor
+			{
+				console.log(item.name + " is armor");
+				stock = shopStock[currentShop]['armorBaseStock'][item.id];
+			}
+		}
+		
+		return stock;
 	}
 	
 	function changeCurrentStock(shopName, itemType, itemId, stock)
 	{
 		if (itemType == 'item' || itemType == 'weapon' || itemType == 'armor')
 		{
-			shopStock[shopName][itemType+'Stock'][itemId] += parseInt(stock);
+			shopStock[shopName][itemType+'Stock'][parseInt(itemId)] += parseInt(stock);
 		}
 	}
 	
@@ -105,17 +179,23 @@ Where stock has not been defined, should the shop act as usual with infinite sto
 	{
 		if (itemType == 'item' || itemType == 'weapon' || itemType == 'armor')
 		{
-			shopStock[shopName][itemType+'BaseStock'][itemId] += parseInt(stock);
+			shopStock[shopName][itemType+'BaseStock'][parseInt(itemId)] += parseInt(stock);
 		}
 	}
 	
-	function getItemStock(item)
+	// gets the item, but not currently returning stock properly
+	getItemStock = function(shop, item)
 	{
-		stock = 0;
+		var stock = undefined;
+		
+		stock = getCurrentStock(shop, item);
+		
+		console.log(stock);
+		
 		return stock;
 	}
 	
-	// overwrite; need to draw stock, too
+	// overwrite; need to draw stock, but drawItemName is used elsewhere, so we're replacing the use of drawItemName with drawShopItemName
 	var oldDrawItem = Window_ShopBuy.prototype.drawItem;
 	Window_ShopBuy.prototype.drawItem = function(index) {
 		var item = this._data[index];
@@ -135,7 +215,15 @@ Where stock has not been defined, should the shop act as usual with infinite sto
 			var iconBoxWidth = Window_Base._iconWidth + 4;
 			this.resetTextColor();
 			this.drawIcon(item.iconIndex, x + 2, y + 2);
-			this.drawText(item.name + " (" + getItemStock(item) + ")", x + iconBoxWidth, y, width - iconBoxWidth);
+			var stock = getItemStock(currentShop, item);
+			if (!(stock === undefined))
+			{
+				this.drawText(item.name + " (" + stock + ")", x + iconBoxWidth, y, width - iconBoxWidth);
+			}
+			else
+			{
+				this.drawText(item.name, x + iconBoxWidth, y, width - iconBoxWidth);
+			}
 		}
 	};
 	
